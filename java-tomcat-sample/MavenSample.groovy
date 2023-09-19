4494272f1210df001e8e1e01b9cd7e5c18881ddd
Syed Haseeb Ahmed <96917352+haseebahmedsyed@users.noreply.github.com>
2023-09-20 01:23:09 +0500

    Create MavenSample.groovy

---
+++ java-tomcat-sample/MavenSample.groovy
@@ -0,0 +1,15 @@
+job('java-tomcat-sample-job'){
+  description('This is the code dsl file which will create a job in jenkins')
+  scm{
+    git('https://github.com/haseebahmedsyed/Jenkins_Upgradev3.git','master')
+  }
+  triggers{
+    scm('* * * * *')
+  }
+  steps{
+    maven('clean package','java-tomcat-sample/pom.xml')
+  }
+  publishers{
+    archiveArtifacts '**/*.war'
+  }
+}
